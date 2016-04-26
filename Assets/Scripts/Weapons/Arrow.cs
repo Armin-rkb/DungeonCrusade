0b0b223aa174d88b6a7a7f7407d2631ec4d1c6cc
DasArmin <armin.hunter@hotmail.com>
2016-04-26 12:03:46 +0200

    Fixed Bow script with knockback

---
+++ Assets/Scripts/Weapons/Arrow.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rbArrow;
+    private bool isRight;
+    private bool isLeft;
+
+    //Sets the place the player is facing
+    public void ShootLeft()
+    {
+        isLeft = true;
+    }
+
+    //Sets the place the player is facing
+    public void ShootRight()
+    {
+        isRight = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (isRight)
+            rbArrow.velocity = new Vector2(10, rbArrow.velocity.y);
+        else if (isLeft)
+            rbArrow.velocity = new Vector2(-10, rbArrow.velocity.y);
+    }
+
+    void Hit(GameObject player)
+    {
+        //HP moet eraf
+        //Give knockback
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        rb.AddForce(player.transform.right * 500);
+        print("push");
+    }
+    
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject != null)
+        {
+            if (coll.gameObject.tag == "Player")
+            {
+                Hit(coll.gameObject);
+                Destroy(this.gameObject);
+            }
+            else
+                Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/DefaultBow.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DefaultBow : MonoBehaviour, IWeapons
+{
+    [SerializeField] private GameObject arrow;
+    private Arrow arrowScript;
+    private PlayerFlip flip;
+    private float cooldown = 0;
+
+    void Awake()
+    {
+        flip = GetComponent<PlayerFlip>();
+    }
+
+    public void Attack()
+    {
+        if (flip.facingRight)
+        {
+            GameObject currArrow = Instantiate(arrow, new Vector2(transform.position.x + 1, transform.position.y), arrow.transform.rotation)as GameObject;
+            currArrow.GetComponent<Arrow>().ShootRight();
+        }
+        else
+        {
+            GameObject currArrow = Instantiate(arrow, new Vector2(transform.position.x - 1, transform.position.y), arrow.transform.rotation) as GameObject;
+            currArrow.GetComponent<Arrow>().ShootLeft();
+        }
+    }
+
+    void Update()
+    {
+        if (cooldown <= 60)
+        {
+            cooldown--;
+
+            if (cooldown <= 1)
+                cooldown = 0;
+        }
+
+        if (Input.GetMouseButtonDown(0) && cooldown == 0)
+        {
+            cooldown = 60;
+            Attack();
+        }
+    }
+}
